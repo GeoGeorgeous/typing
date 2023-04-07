03240cf40c4e7cd369514f344a8b20ea0a373129
George Gorchev <g.gorchev@xbprime.com>
2023-04-07 07:42:50 +0000

    add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import '../styles/app.sass';
+</script>
+
+<svelte:head>
+  <title>Typing</title>
+</svelte:head>
+
+<div class="layout">
+  <nav><h1>🔥 typing</h1></nav>
+  <main><slot /></main>
+</div>
+
+<style lang="sass">
+   h1
+    font-family: 'Lexend Deca', sans-serif
+    font-size: 1.8rem
+    color: var(--fg-200)
+    letter-spacing: 2px
+
+
+  .layout
+    height: 100%
+    display: grid
+    grid-template-rows: auto 1fr
+    align-items: center
+    padding: 2rem
+
+</style>
